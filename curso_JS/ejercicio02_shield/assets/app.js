27fcc08c5d667a1b09400f93dd968603a1354005
vicpack <childrenofpunk@hotmail.com>
2021-03-27 19:09:19 -0600

    ejercicio-escudo

---
+++ curso_JS/ejercicio02_shield/assets/app.js
@@ -0,0 +1,89 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function calculationResult(calculationType = 'SUBTRACT') {
+    const enteredNumber = getUserNumberInput();
+    if (
+        (calculationType !== 'ADD' &&
+            calculationType !== 'SUBTRACT' &&
+            calculationType !== 'MULTIPLY' &&
+            calculationType !== 'DIVIDE') ||
+        !enteredNumber
+    ) {
+        return;
+    }
+
+    if (
+        calculationType === 'ADD' ||
+        calculationType === 'SUBTRACT' ||
+        calculationType === 'MULTIPLY' ||
+        calculationType === 'DIVIDE'
+    ) {
+        const initialResult = currentResult;
+
+        let mathOperator;
+        if (calculationType === 'ADD') {
+            currentResult += enteredNumber;
+            mathOperator = '+';
+        } else if (calculationType === 'SUBTRACT') {
+            currentResult -= enteredNumber;
+            mathOperator = '-';
+        } else if (calculationType === 'MULTIPLY') {
+            currentResult -= enteredNumber;
+            mathOperator = '*';
+        } else if (calculationType === 'DIVIDE') {
+            currentResult -= enteredNumber;
+            mathOperator = '/';
+        }
+
+        createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+        writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+    }
+}
+
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult
+) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult,
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function add() {
+    calculationResult('ADD');
+}
+
+function subtract() {
+    calculationResult('SUBTRACT');
+}
+
+function multiply() {
+    calculationResult('MULTIPLY');
+}
+
+function divide() {
+    calculationResult('DIVIDE2');
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
